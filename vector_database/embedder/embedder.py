a6882a8196ff50f392ba1c0ed4e2bb13c96349b4
HunorHajdu <hajduhunor@yahoo.com>
2024-12-07 15:51:45 +0200

    Refactor Embedder class to support variable vector dimensions

--- vector_database/embedder/embedder.py
+++ vector_database/embedder/embedder.py
@@ -1,8 +1,19 @@
 from sentence_transformers import SentenceTransformer
+from sklearn.decomposition import PCA
 
 class Embedder:
-    def __init__(self, model_name="all-MiniLM-L6-v2"):
+    def __init__(self, model_name="all-MiniLM-L6-v2", vec_dim=384):
         self.model = SentenceTransformer(model_name)
+        self.vec_dim = vec_dim
+        self.pca = PCA(n_components=vec_dim)
 
     def embed(self, text):
-        return self.model.encode(text)
+        vectors = self.model.encode(text, convert_to_numpy=True)
+        
+        if vectors.ndim == 1:
+            vectors = vectors.reshape(1, -1)
+
+        if vectors.shape[1] != self.vec_dim:
+            vectors = self.pca.fit_transform(vectors)
+
+        return vectors
--- vector_database/vector_database.py
+++ vector_database/vector_database.py
@@ -1,15 +1,27 @@
-import faiss
 from vector_database.embedder.embedder import Embedder
 
+import numpy as np
+import hnswlib
+
 class VectorDatabase:
-    def __init__(self, vec_dim=512, embedder=None):
-        self.index = faiss.IndexFlatL2(vec_dim)
-        self.embedder = Embedder() if embedder is None else embedder
+    def __init__(self, embedder=None, vec_dim=384, documents=None):
+        self.embedder = Embedder(vec_dim=vec_dim) if embedder is None else embedder
+        self.index = hnswlib.Index(space='cosine', dim=vec_dim)
+        self.index.init_index(max_elements=100000, ef_construction=32, M=16)
+        self.index.set_ef(16)
+        self.documents = documents
 
     def add_vectors(self, text):
         vectors = self.embedder.embed(text)
-        self.index.add(vectors)
+        self.index.add_items(vectors, np.arange(len(vectors)))
+
 
     def search_vector(self, text, k=1):
         vector = self.embedder.embed(text)
-        return self.index.search(vector, k)
+        
+        results = []
+        labels, distances = self.index.knn_query(vector, k=k)
+        for label, distance in zip(labels[0], distances[0]):
+            results.append({"label": label, "distance": distance, 'vector': self.index.get_items([label])[0], 'text': self.documents[label]})        
+
+        return results  
