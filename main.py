3607825ffc26d13d250ff408659668257968fca9
HunorHajdu <hajduhunor@yahoo.com>
2024-12-07 14:25:29 +0200

    Add vector database and integrate with dataset loading logic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from local_datasets.data_EN.data_en import DataEN
 from local_datasets.data_HU.data_hu import DataHU
 from local_datasets.data_RO.data_ro import DataRO
 from ocr.ocr import OCRModel
+from vector_database.vector_database import VectorDatabase
 
 from tqdm import tqdm
 import numpy as np
@@ -15,8 +16,10 @@ if __name__ == "__main__":
     # dataset_classes = [DataEN, DataHU, DataRO]
     dataset_names = ["HU"]
     dataset_classes = [DataHU]
+
     dataset_split = "train"
     limit = 5
+
     ocr_model = "easyocr"
     
     for name, cls in zip(dataset_names, dataset_classes):
@@ -39,3 +42,9 @@ if __name__ == "__main__":
 
     for dataset in datasets:
         dataset = dataset.map(ocr.apply_ocr)
+
+    vector_database = VectorDatabase()
+    for dataset in datasets:
+        for key in dataset.keys():
+            for text in tqdm(dataset[key]):
+                vector_database.add_vectors(text)
